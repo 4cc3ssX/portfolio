5ca02c524a66c60e922a1feba3dc4abaad263e41
Ryam <4cc3ssflick@gmail.com>
2024-10-06 18:54:00 +0700

    refactor: loading screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,46 +1,40 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  style: "normal",
-  variable: "--font-Poppins",
-});
 
 export default function Loading() {
   return (
     <div
-      className={`${poppins.className} w-screen h-screen flex flex-col justify-center items-center`}
+      className="w-screen h-screen flex flex-col justify-center items-center font-sans"
     >
       <div className="flex flex-col items-center gap-2">
         <motion.p
-          className="font-sans text-base text-white"
+          className="text-base"
           animate={{
-            opacity: [0.2, 0.4, 1],
+            opacity: [0.4, 0.7, 1],
           }}
           transition={{
             repeat: Infinity,
-            duration: 1.2,
-            ease: "linear",
+            repeatType: "reverse",
+            duration: 1.5,
+            ease: "easeInOut",
           }}
         >
-          Loading...
+          Getting ready...
         </motion.p>
-        <div className="relative w-40 h-1 rounded-full bg-bar overflow-hidden">
+        <div className="relative w-48 h-0.5 rounded-ful bg-secondary-foreground overflow-hidden">
           <motion.div
-            className="absolute inset-y-0 left-0 right-0 bg-primary"
+            className="absolute inset-y-0 left-0 right-0 bg-foreground rounded-full"
             initial={{
               width: "0%",
             }}
             animate={{
-              width: ["0%", "75%", "100%"],
+              width: ["0%", "70%", "100%"],
               x: ["0%", "45%", "100%"],
             }}
             transition={{
               repeat: Infinity,
+              repeatType: "loop",
               duration: 1.2,
               ease: "linear",
             }}
