73a10e7b8c1f35b000142112722db282dd0638e1
Ryam <4cc3ssflick@gmail.com>
2024-10-08 18:59:52 +0700

    feat: landing + experience page renovation

---
+++ src/app/_pages/Experience/_components/experience-card.tsx
@@ -0,0 +1,63 @@
+import { ExperienceWithCompany } from "@/shared/db/schema";
+import { openURL } from "@/utils";
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+
+export interface ExperienceCardProps {
+  experience: ExperienceWithCompany;
+}
+
+export const ExperienceCard = ({ experience }: ExperienceCardProps) => {
+  return (
+    <div className="flex flex-row items-center gap-x-3 py-2.5">
+      <div className="hidden md:block border border-muted rounded-full p-0.5">
+        <Image
+          placeholder={experience.company.image.blurHash as any}
+          src={experience.company.image.uri}
+          alt={`${experience.company.name} logo`}
+          width={32}
+          height={32}
+          className="object-center rounded-full"
+        />
+      </div>
+      <div className="flex-[2] flex flex-col gap-y-1">
+        <div className="flex flex-row items-center gap-x-2">
+          <Link
+            aria-disabled={!experience.company.uri}
+            href={experience.company.uri ?? "#"}
+            target="_blank"
+            className="font-medium text-lg hover:underline underline-offset-2"
+          >
+            {experience.company.name}
+          </Link>
+          {experience.company.uri ? (
+            <button
+              title={`${experience.company.name}`}
+              onClick={() => openURL(experience.company.uri!, true)}
+            >
+              <BsBoxArrowUpRight />
+            </button>
+          ) : null}
+        </div>
+        <p className="text-sm text-muted-foreground">{experience.position}</p>
+      </div>
+      <div className="flex flex-row items-center justify-start gap-2">
+        <p className="text-sm text-muted-foreground text-left">
+          {dayjs(experience.startAt).format("MMM YYYY")}
+          {" - "}
+          <span
+            className={`${
+              experience.isActive ? "text-primary" : "text-muted-foreground"
+            }`}
+          >
+            {experience.isActive
+              ? "Present"
+              : dayjs(experience.endAt).format("MMM YYYY")}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/_pages/Landing/index.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { handleNavigate, openURL } from "@/utils";
+import { UserWithLinks } from "@/shared/db/schema";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  data: UserWithLinks;
+}
+
+const Landing = ({ data }: Props) => {
+  const resumeLink = data.links.find((link) => link.name.match(/resume/i));
+  return (
+    <div id="landing" className="relative flex pt-14 min-h-dvh">
+      <div className="flex flex-1 flex-col justify-center items-center">
+        <motion.div
+          initial={{
+            y: 20,
+            filter: "blur(8px)",
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            filter: "blur(0px)",
+            opacity: 1,
+          }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="flex flex-col gap-y-4 w-full md:w-4/6 lg:w-7/12 px-6 will-change-auto"
+        >
+          <div className="flex flex-col gap-y-3">
+            <p className="text-sm md:text-base">Hi, I&apos;m {data.nickname}!</p>
+            <p className="font-bold text-4xl md:text-5xl">{data.slogan}</p>
+            <p className="text-sm md:text-base">
+              I'm a full-stack software engineer based in{" "}
+              <span className="underline cursor-pointer">Bangkok</span>, who
+              loves at breaking things, building cutting-edge, accessible and
+              well-optimized apps.
+            </p>
+          </div>
+          <div className="mt-6 sm:mt-1 flex flex-row gap-x-2">
+            <Button
+              className="rounded-full"
+              onClick={() => handleNavigate("#about")}
+            >
+              Let&apos;s Get Started 👋🏻
+            </Button>
+            {resumeLink ? (
+              <Button
+                variant="link"
+                onClick={() => openURL(resumeLink.uri, true)}
+              >
+                Download Resume
+              </Button>
+            ) : null}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
