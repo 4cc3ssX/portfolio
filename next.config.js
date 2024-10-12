9435d15a19ad7c6d8490c2429c9754700d60998e
Ryam <4cc3ssflick@gmail.com>
2024-10-12 22:00:26 +0700

    chore: remove console statement on production

--- next.config.js
+++ next.config.js
@@ -43,6 +43,11 @@ const nextConfig = {
       },
     ],
   },
+  compiler: {
+    // Remove console logs only in production, excluding error logs
+    removeConsole:
+      process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+  },
 };
 
 module.exports = nextConfig;
