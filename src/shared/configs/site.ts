84dc36039be805bb70e53e602017c78f57ffdc67
Ryam <4cc3ssflick@gmail.com>
2024-10-13 17:17:41 +0700

    fix: update sitemap config

--- src/shared/configs/site.ts
+++ src/shared/configs/site.ts
@@ -1,6 +1,6 @@
 export const configs = {
   url:
     process.env.NODE_ENV === "production"
-      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-      : `https://${process.env.VERCEL_URL}`,
+      ? `https://ryam.me`
+      : `http://localhost:3000`,
 };
