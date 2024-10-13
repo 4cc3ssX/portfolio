42803d8fb9e239ddfa0e0609b0fc99bfd020ae3c
Ryam <4cc3ssflick@gmail.com>
2024-10-13 11:46:56 +0700

    fix: minor changes

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,21 +6,18 @@ import Contact from "./_pages/Contact";
 import Experience from "./_pages/Experience";
 import Landing from "./_pages/Landing";
 import Projects from "./_pages/Projects";
-import {
-  ExperienceWithCompany,
-  ProjectWithLinkAndTags,
-  UserWithLinks,
-} from "@/shared/db/schema";
+import { ExperienceWithCompany, UserWithLinks } from "@/shared/db/schema";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { useNavigation } from "@/shared/hooks/use-navigation";
 import Skills from "./_pages/Skills";
 import { SkillWithTag } from "@/shared/db/schema/skills";
+import { ProjectWithLinkAndTagsWithGithubData } from "@/actions/projects";
 
 interface Props {
   me: UserWithLinks;
   experiences: ExperienceWithCompany[];
-  projects: ProjectWithLinkAndTags[];
+  projects: ProjectWithLinkAndTagsWithGithubData[];
   skills: SkillWithTag[];
 }
 
